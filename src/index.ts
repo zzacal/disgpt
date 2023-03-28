eadde9ca92c715bd3dbb35061a916362cdd9c6b9
zzacal <jizacal@gmail.com>
2023-03-27 17:43:16 -0700

    Improves configuration checking and error

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import 'dotenv/config';
-import { Builder } from './builder';
+import { ExpressAppBuilder } from './builder';
 import { AIService } from './ai/ai-service';
 import { Configuration, OpenAIApi } from 'openai';
 
-const APPID = process.env.APP_ID!;
-const PUBLIC_KEY = process.env.PUBLIC_KEY!
-const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN!;
+const APPID = process.env.APP_ID;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const OPEN_AI_API = new OpenAIApi(new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: OPENAI_API_KEY,
 }));
 const CHAT_SERVICE = new AIService(OPEN_AI_API);
 
 const PORT = process.env.PORT || 3000;
 
+if (APPID && PUBLIC_KEY && DISCORD_BOT_TOKEN && OPENAI_API_KEY) {
   (new ExpressAppBuilder())
     .build(CHAT_SERVICE, APPID, PUBLIC_KEY, DISCORD_BOT_TOKEN)
     .then( app => app.listen(PORT, () => console.log(`app: ${APPID}\nlistening: ${PORT}`)))
+} else {
+  throw new Error("disgpt is misconfigured.\nAPPID && PUBLIC_KEY && DISCORD_BOT_TOKEN && OPENAI_API_KEY are all required, but at least one is of them is missing.")
+}
